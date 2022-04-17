38f02841d8726435ab9defa06c6337c4d554961f
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-04-17 17:06:54 -0500

    Iterando los datos obtenidos de la API del clima y seteandolos a los componentes HTML.

--- index.js
+++ index.js
@@ -10,12 +10,26 @@ const API_KEY = "73e7de11543d31d67142354335c5930e";
  */
 const fetchData = (position) => {
   const { latitude, longitude } = position.coords; // Obteniendo las coordenadas del usuario.
-  fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+  )
     .then((response) => response.json()) // Transformando la información a formato json.
     .then((data) => setWeatherData(data)); // Seteando la data a la función encargada de procesarla.
 };
 
-const setWeatherData = (data) => {};
+const setWeatherData = (data) => {
+  const weatherData = {
+    location: data.name,
+    description: data.weather[0].main,
+    humidity: data.main.humidity,
+    pressure: data.main.pressure,
+    temperature: data.main.temp,
+    date: "data",
+  };
+  Object.keys(weatherData).forEach((key) => {
+    document.getElementById(key).textContent = weatherData[key];
+  });
+};
 
 /**
  * Obtiene la localización del usuario en cuestión.
